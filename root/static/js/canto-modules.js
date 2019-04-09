9b9c67c5edf34da13a156f3a2a1bf39b10f0fa69
James Seager <james.seager@rothamsted.ac.uk>
2019-04-09 14:13:27 +0100

    Add function makeTaxonGenotypeMap to metagenotypeManage
    Returns an object indexed by the taxon IDs of every organism with at 
    least one genotype, where the key values are the genotypes corresponding 
    to the taxon ID.

--- root/static/js/canto-modules.js
+++ root/static/js/canto-modules.js
@@ -7244,6 +7244,14 @@ var metagenotypeManage = function (CantoGlobals, Curs, CursGenotypeList, Metagen
       };
 
       StrainsService.getAllSessionStrains();
+
+      function makeTaxonGenotypeMap(genotypes) {
+        var getGenotypeTaxonId = function (genotype) {
+          return genotype.organism.taxonid;
+        };
+        return indexArray(genotypes, getGenotypeTaxonId);
+      }
+
     }
   };
 };
